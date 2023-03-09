5806e7d0f5d28168bdea062acdccb6782fd2b439
Miguel-T24 <miguelt2296@gmail.com>
2023-03-09 12:29:12 -0500

    Add Doc comments

--- main.py
+++ main.py
@@ -5,3 +5,14 @@ app = FastAPI()
 @app.get('/')
 async def root():
     return {'message': 'hello'}
+
+
+
+
+# To View the auto doc with swagger
+# route + /docs
+# Example http://127.0.0.1:8000/docs
+
+# or with redoc
+# route + /ReDoc
+# Example: http://127.0.0.1:8000/redoc
