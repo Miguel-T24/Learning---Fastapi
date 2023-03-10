b51603c743eceebd2c5dced3aaf8f3521d714621
Miguel-T24 <miguelt2296@gmail.com>
2023-03-09 23:01:49 -0500

    Add Path paramenters concept

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+# To View the auto doc with swagger
+# route + /docs
+# Example http://127.0.0.1:8000/docs
+
+# or with redoc
+# route + /ReDoc
+# Example: http://127.0.0.1:8000/redoc
+
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,13 +15,12 @@ app = FastAPI()
 async def root():
     return {'message': 'hello'}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id" : item_id}
 
+# parameters with types
+@app.get("/types/{item_id}")
+async def read_item(item_id : int):
+    return{"item_id" : item_id}
 
-
-# To View the auto doc with swagger
-# route + /docs
-# Example http://127.0.0.1:8000/docs
-
-# or with redoc
-# route + /ReDoc
-# Example: http://127.0.0.1:8000/redoc
